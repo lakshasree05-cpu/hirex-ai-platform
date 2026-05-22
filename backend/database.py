eff2bace7caf518c7c117ea33575791914a018d4
Laksha <lakshasree05@gmail.com>
2026-05-23 00:04:42 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
+DATABASE_URL = "sqlite:///./hirex.db"
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
